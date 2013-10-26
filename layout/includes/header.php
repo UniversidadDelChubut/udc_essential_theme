94b44a071143ca829c76d96dc27c531cf3b0b5a6
mppfiles <mppfiles@gmail.com>
2013-10-26 11:45:16 -0300

    cierro #1

--- layout/includes/header.php
+++ layout/includes/header.php
@@ -113,37 +113,37 @@ if (strpos($checkuseragent, 'MSIE 8') || strpos($checkuseragent, 'MSIE 7')) {?>
         <p id="socialheading"><?php echo get_string('socialnetworks','theme_udcessential')?></p>
             <ul class="socials unstyled">
                 <?php if ($haswebsite) { ?>
-                <li><a href="<?php echo $haswebsite; ?>" class="website"><span class="icon-stack"><i class="icon-sign-blank icon-stack-base"></i><i class="icon-globe icon-light"></i></span></a></li>
+                <li><a href="<?php echo $haswebsite; ?>" target="_blank" class="website"><span class="icon-stack"><i class="icon-sign-blank icon-stack-base"></i><i class="icon-globe icon-light"></i></span></a></li>
                 <?php } ?>
                 <?php if ($hasgoogleplus) { ?>
-                <li><a href="<?php echo $hasgoogleplus; ?>" class="googleplus"><i class="icon-google-plus-sign"></i></a></li>
+                <li><a href="<?php echo $hasgoogleplus; ?>" target="_blank" class="googleplus"><i class="icon-google-plus-sign"></i></a></li>
                 <?php } ?>
                 <?php if ($hastwitter) { ?>
-                <li><a href="<?php echo $hastwitter; ?>" class="twitter"><i class="icon-twitter-sign"></i></a></li>
+                <li><a href="<?php echo $hastwitter; ?>" target="_blank" class="twitter"><i class="icon-twitter-sign"></i></a></li>
                 <?php } ?>
                 <?php if ($hasfacebook) { ?>
-                <li><a href="<?php echo $hasfacebook; ?>" class="facebook"><i class="icon-facebook-sign"></i></a></li>
+                <li><a href="<?php echo $hasfacebook; ?>" target="_blank" class="facebook"><i class="icon-facebook-sign"></i></a></li>
                 <?php } ?>
                 <?php if ($haslinkedin) { ?>
-                <li><a href="<?php echo $haslinkedin; ?>" class="linkedin"><i class="icon-linkedin-sign"></i></a></li>
+                <li><a href="<?php echo $haslinkedin; ?>" target="_blank" class="linkedin"><i class="icon-linkedin-sign"></i></a></li>
                 <?php } ?>
                 <?php if ($hasyoutube) { ?>
-                <li><a href="<?php echo $hasyoutube; ?>" class="youtube"><i class="icon-youtube-sign"></i></a></li>
+                <li><a href="<?php echo $hasyoutube; ?>" target="_blank" class="youtube"><i class="icon-youtube-sign"></i></a></li>
                 <?php } ?>
                 <?php if ($hasflickr) { ?>
-                <li><a href="<?php echo $hasflickr; ?>" class="flickr"><i class="icon-flickr"></i></a></li>
+                <li><a href="<?php echo $hasflickr; ?>" target="_blank" class="flickr"><i class="icon-flickr"></i></a></li>
                 <?php } ?>
                 <?php if ($haspinterest) { ?>
-                <li><a href="<?php echo $haspinterest; ?>" class="pinterest"><i class="icon-pinterest-sign"></i></a></li>
+                <li><a href="<?php echo $haspinterest; ?>" target="_blank" class="pinterest"><i class="icon-pinterest-sign"></i></a></li>
                 <?php } ?>
                 <?php if ($hasinstagram) { ?>
-                <li><a href="<?php echo $hasinstagram; ?>" class="instagram"><span class="icon-stack"><i class="icon-sign-blank icon-stack-base"></i><i class="icon-instagram icon-light"></i></span></a></li>
+                <li><a href="<?php echo $hasinstagram; ?>" target="_blank" class="instagram"><span class="icon-stack"><i class="icon-sign-blank icon-stack-base"></i><i class="icon-instagram icon-light"></i></span></a></li>
                 <?php } ?>
                 <?php if ($hasvk) { ?>
-                <li><a href="<?php echo $hasvk; ?>" class="vk"><span class="icon-stack"><i class="icon-sign-blank icon-stack-base"></i><i class="icon-vk icon-light"></i></span></a></li>
+                <li><a href="<?php echo $hasvk; ?>" target="_blank" class="vk"><span class="icon-stack"><i class="icon-sign-blank icon-stack-base"></i><i class="icon-vk icon-light"></i></span></a></li>
                 <?php } ?>
                 <?php if ($hasskype) { ?>
-                <li><a href="<?php echo $hasskype; ?>" class="skype"><span class="icon-stack"><i class="icon-sign-blank icon-stack-base"></i><i class="icon-skype icon-light"></i></span></a></li>
+                <li><a href="<?php echo $hasskype; ?>" target="_blank" class="skype"><span class="icon-stack"><i class="icon-sign-blank icon-stack-base"></i><i class="icon-skype icon-light"></i></span></a></li>
                 <?php } ?>
 	    </ul>
         </div>
